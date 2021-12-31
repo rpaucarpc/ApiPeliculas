d1db1e21c6306bc88af7445cd9ba55c506c3e498
rpaucarc <rpaucarpc@gmail.com>
2021-12-31 14:50:20 -0500

    Creacion de repositorio categoria

---
+++ ApiPeliculas/Repository/CategoriaRepositorio.cs
@@ -0,0 +1,63 @@
+﻿using ApiPeliculas.Data;
+using ApiPeliculas.Models;
+using ApiPeliculas.Repository.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiPeliculas.Repository
+{
+    public class CategoriaRepositorio : ICategoriaRepository
+    {
+        private readonly ApplicationDbContext _bd;
+        public CategoriaRepositorio(ApplicationDbContext bd)
+        {
+            _bd = bd;
+        }
+        public bool ActualizarCategoria(Categoria categoria)
+        {
+            _bd.Categorias.Update(categoria);
+            return Guardar();
+        }
+
+        public bool BorrarCategoria(Categoria categoria)
+        {
+            _bd.Categorias.Remove(categoria);
+            return Guardar();
+        }
+
+        public bool CrearCategoria(Categoria categoria)
+        {
+            _bd.Categorias.Add(categoria);
+            return Guardar();
+        }
+
+        public bool ExisteCategoria(string nombreCategoria)
+        {
+            bool valor = _bd.Categorias.Any(c => c.Nombre.ToLower().Trim() == nombreCategoria.ToLower().Trim());
+            return valor;
+        }
+
+        public bool ExisteCategoria(int categoriaId)
+        {
+            bool valor = _bd.Categorias.Any(c => c.Id == categoriaId);
+            return valor;
+        }
+
+        public Categoria GetCategoria(int categoriaId)
+        {
+            return _bd.Categorias.FirstOrDefault(c=>c.Id == categoriaId);
+        }
+
+        public ICollection<Categoria> GetCategorias()
+        {
+            return _bd.Categorias.OrderBy(c=>c.Nombre).ToList();
+        }
+
+        public bool Guardar()
+        {
+            return _bd.SaveChanges() >= 0;
+        }
+    }
+}
---
+++ ApiPeliculas/Repository/IRepository/ICategoriaRepository.cs
@@ -0,0 +1,20 @@
+﻿using ApiPeliculas.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiPeliculas.Repository.IRepository
+{
+    public interface ICategoriaRepository
+    {
+        ICollection<Categoria> GetCategorias();
+        Categoria GetCategoria(int categoriaId);
+        bool ExisteCategoria(string nombreCategoria);
+        bool ExisteCategoria(int categoriaId);
+        bool CrearCategoria(Categoria categoria);
+        bool ActualizarCategoria(Categoria categoria);
+        bool BorrarCategoria(Categoria categoria);
+        bool Guardar();
+    }
+}
