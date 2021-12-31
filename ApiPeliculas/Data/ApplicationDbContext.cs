b3f214ac49a99d40bada0c6d237d9f45e65c5e5d
rpaucarc <rpaucarpc@gmail.com>
2021-12-30 22:54:58 -0500

    Add project files.

---
+++ ApiPeliculas/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using ApiPeliculas.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiPeliculas.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext>options) : base(options)
+        {
+
+        }
+
+        public DbSet<Categoria>Categorias{ get; set; }
+    }
+}
